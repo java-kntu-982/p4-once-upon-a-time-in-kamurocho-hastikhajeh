99565707038e95a72c21161f1d204307799f112b
hastikhajeh <hastikh6.11@gmail.com>
2020-06-18 01:45:06 +0430

    added new enemies

--- src/main/java/ir/ac/kntu/soldier/enemy/GoldSoldier.java
+++ src/main/java/ir/ac/kntu/soldier/enemy/GoldSoldier.java
@@ -1,4 +1,11 @@
 package ir.ac.kntu.soldier.enemy;
 
-public class GoldSoldier {
+import ir.ac.kntu.soldier.EnemySoldier;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class GoldSoldier extends EnemySoldier {
+    public GoldSoldier(double x, double y) {
+        super(x, y, 800, 2000, 5, new Rectangle(17, 23, Color.GOLD));
+    }
 }
--- src/main/java/ir/ac/kntu/soldier/enemy/GreenSoldier.java
+++ src/main/java/ir/ac/kntu/soldier/enemy/GreenSoldier.java
@@ -1,4 +1,11 @@
 package ir.ac.kntu.soldier.enemy;
 
-public class GreenSoldier {
+import ir.ac.kntu.soldier.EnemySoldier;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class GreenSoldier extends EnemySoldier {
+    public GreenSoldier(double x, double y) {
+        super(x, y, 1500, 700, 1, new Rectangle(11, 15, Color.GREEN));
+    }
 }
